cf75fafe0dbb1414313dee896a5bb2b9c1272f10
Zhuangda Zhu <zhuangdaz@gmail.com>
2021-04-17 23:42:40 -0400

    Landing Page: ssetup and styling

--- src/views/Root.vue
+++ src/views/Root.vue
@@ -1,38 +1,55 @@
 <template>
-  <div>
-    <h1>Root</h1>
-    <amplify-authenticator usernameAlias="email">
-      <amplify-sign-out/>
-    </amplify-authenticator>
+  <div class="flex w-full h-screen">
+    <!-- left side  -->
+    <div class="flex w-1/2 h-full bg-blue">
+      <div class="flex items-center justify-center w-full h-full">
+        <div class="flex flex-col">
+          <div class="flex items-center">
+            <i class="fas fa-search text-white text-2xl mr-5"></i>
+            <p class="text-white font-bold text-xl">Follow your interest.</p>
+          </div>
+          <div class="flex items-center mt-10">
+            <i class="fas fa-user-friends text-white text-2xl mr-4"></i>
+            <p class="text-white font-bold text-xl">Hear what people are talking about.</p>
+          </div>
+          <div class="flex items-center mt-10">
+            <i class="fas fa-comment text-white text-2xl mr-5"></i>
+            <p class="text-white font-bold text-xl">Join the converstation.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- right side -->
+    <div class="flex w-1/2 h-full">
+      <div class="flex flex-col w-1/2 font-bold">
+        <i class="fab fa-twitter text-blue text-4xl mb-5"></i>
+        <p class="text-3xl mb-12">See what's happening in the world right now</p>
+        <p>Join Twitter today.</p>
+        <button class="rounded-full bg-blue font-bold text-lg text-white mt-4 p-3 hover:bg-darkblue">Sign up</button>
+        <button @click.prevent="showSignInPage" class="rounded-full border border-blue bg-white font-bold text-lg text-blue mt-4 p-3 hover:bg-lightblue">Log in</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
-import { mapActions, mapGetters } from 'vuex'
-
 export default {
   name: 'Root',
-  computed: {
-    ...mapGetters('authentication', [
-      'loggedIn',
-      'user'
-    ])
-  },
   methods: {
-    ...mapActions('authentication', [
-      'loginUser',
-      'logoutUser'
-    ])
-  },
-  created() {
-    onAuthUIStateChange(async (state, user) => {
-      if (state === AuthState.SignedIn) {
-        await this.loginUser(user)
-      } else if (state === AuthState.SignedOut) {
-        await this.logoutUser()
-      }
-    })
+    showSignInPage() {
+      this.$router.push('Login')
+    }
   }
 }
 </script>
+
+<style>
+div {
+  border: 1px dashed blue;
+  padding: 15px;
+}
+div.items-center.justify-center {
+  background-color: rgba(240, 240, 240, 0.5);
+}
+
+</style>
